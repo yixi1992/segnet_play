2b49b64f4dcb223d7ce2f0b119a7866fbde559e2
yixi1992 <yixi1992@gmail.com>
2016-04-26 18:34:32 -0400

    inference

---
+++ segnet_basic_results/inf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -t 1:00:00
+#SBATCH -N 1
+#SBATCH --mail-user=yixi@cs.umd.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --job-name="segnetbasicinflmdb"
+#SBATCH --partition="scavenger"
+
+
+python ../Scripts/test_segmentation_camvid.py --model segnet_basic_inference.prototxt --weights ../Models/Inference/segnet_basic_camvid.caffemodel --iter 233
+
